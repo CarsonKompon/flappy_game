cf15b6f3697e175e6771bc5ceede930c59e811c0
Carson Kompon <carsokompo@gmail.com>
2023-08-31 22:20:13 -0400

    Networking + player movement

---
+++ code/Player/Player.cs
@@ -0,0 +1,84 @@
+using Sandbox;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+using Sandbox.Menu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+
+namespace FlappyGame;
+
+[StyleSheet]
+public class Player : Panel
+{
+    public static float GRAVITY = 1600f;
+    public float PixelWidth { get => Box.Rect.Width * ScaleFromScreen; }
+    public float PixelHeight { get => Box.Rect.Height * ScaleFromScreen; }
+    
+    
+    public Friend Member { get; set; }
+    public Vector2 Position;
+    public float Vspd = 0f;
+
+
+    Image IconImage;
+    Label NameLabel;
+
+    public Player()
+    {
+        var iconContainer = Add.Panel("icon-container");
+        IconImage = iconContainer.Add.Image("", "icon");
+        NameLabel = Add.Label("", "nametag");
+
+        Position = new Vector2(GameMenu.ScreenWidth/2f, GameMenu.ScreenHeight/3f);
+    }
+
+    public void SetMember(Friend member)
+    {
+        Member = member;
+        IconImage.SetTexture($"avatar:{Member.Id}");
+        NameLabel.Text = Member.Name;
+
+        if(IsLocalPlayer()) AddClass("me");
+    }
+
+	public override void Tick()
+    {
+        Vspd += GRAVITY * Time.Delta;
+
+        if((Input.Pressed("jump") || Input.Pressed("attack1")) && IsLocalPlayer())
+        {
+            Jump();
+        }
+
+        IconImage.SetClass("jumping", Vspd < 0);
+        IconImage.SetClass("falling", Vspd > 0);
+
+        Position.x += 200 * Time.Delta;
+        Position.y += Vspd * Time.Delta;
+
+        Style.Left = Position.x - GameMenu.CameraX;
+        Style.Top = Position.y;
+        
+        if(IsLocalPlayer())
+        {
+            if(Position.y > GameMenu.ScreenHeight - PixelHeight/2 || Position.y < PixelHeight/2)
+            {
+                GameMenu.Kill(Member.Id);
+            }
+        }
+    }
+
+    public void Jump()
+    {
+        Vspd = -625f;
+        GameMenu.Instance.NetworkPlayerUpdate(this);
+    }
+
+    public bool IsLocalPlayer()
+    {
+        return (Member.Id == Game.SteamId);
+    }
+
+}
---
+++ code/UI/GameMenu.Networking.cs
@@ -0,0 +1,83 @@
+using Sandbox;
+using Sandbox.Menu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FlappyGame;
+
+enum NETWORK_MESSAGE
+{
+    PLAYER_SPAWN,
+    PLAYER_UPDATE,
+    PLAYER_KILL
+}
+
+public partial class GameMenu
+{
+    void OnNetworkMessage(ILobby.NetworkMessage msg)
+    {
+        if(msg.Source.Id == Game.SteamId) return;
+
+        ByteStream data = msg.Data;
+
+        ushort messageId = data.Read<ushort>();
+
+        switch((NETWORK_MESSAGE)messageId)
+        {
+            case NETWORK_MESSAGE.PLAYER_SPAWN:
+                SpawnPlayer(msg.Source.Id);
+                break;
+            
+            case NETWORK_MESSAGE.PLAYER_UPDATE:
+
+                bool hasPlayer = false;
+                foreach(var child in GamePanel.Children)
+                {
+                    if(child is Player player && player.Member.Id == msg.Source.Id)
+                    {
+                        hasPlayer = true;
+                        break;
+                    }
+                }
+                if(!hasPlayer) SpawnPlayer(msg.Source.Id);
+
+                ulong x = data.Read<ulong>();
+                ulong y = data.Read<ulong>();
+                float vspd = data.Read<float>();
+                UpdatePlayer(msg.Source.Id, x, y, vspd);
+                break;
+
+            case NETWORK_MESSAGE.PLAYER_KILL:
+                Kill(msg.Source.Id);
+                break;
+        }
+    }
+
+    void NetworkPlayerSpawn()
+    {
+        ByteStream data = ByteStream.Create(2);
+        data.Write((ushort)NETWORK_MESSAGE.PLAYER_SPAWN);
+
+        Lobby.BroadcastMessage(data);
+    }
+
+    public void NetworkPlayerUpdate(Player player)
+    {
+        ByteStream data = ByteStream.Create(22);
+        data.Write((ushort)NETWORK_MESSAGE.PLAYER_UPDATE);
+        data.Write((ulong)player.Position.x);
+        data.Write((ulong)player.Position.y);
+        data.Write((float)player.Vspd);
+
+        Lobby.BroadcastMessage(data);
+    }
+
+    void NetworkPlayerKill()
+    {
+        ByteStream data = ByteStream.Create(2);
+        data.Write((ushort)NETWORK_MESSAGE.PLAYER_KILL);
+
+        Lobby.BroadcastMessage(data);
+    }
+}
